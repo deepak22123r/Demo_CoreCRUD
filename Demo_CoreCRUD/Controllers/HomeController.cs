b7e4d5e76a7c10ed1be8ea8621e22a043af024e9
sajiyakalot148@gmail.com <sajiyakalot148@gmail.com>
2019-12-16 16:08:21 +0530

    delete sql command

---
+++ Demo_CoreCRUD/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Demo_CoreCRUD.Models;
+using Microsoft.Data.SqlClient;
+
+namespace Demo_CoreCRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string Constring = "";
+            SqlConnection con = new SqlConnection(Constring);
+            SqlCommand cmd = new SqlCommand();
+            sqldata
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
